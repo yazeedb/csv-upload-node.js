f3e15aaba64f74c822d21d8ddecf32c4e427ae5a
yazman12 <yazman12@gmail.com>
2015-04-01 23:10:18 -0400

    User can now view their uploaded CSV files with each school's stats

--- app/controllers/getCsv.js
+++ app/controllers/getCsv.js
@@ -1,13 +1,30 @@
-var CSV = require('../models/csv.js'),
-	csv = new CSV();
+var CSV = require('../models/csv.js');
 
 module.exports = function (req, res) {
 
-	CSV.find({}, function (err, data) {
+	CSV.findOne({}, { '_id': 0 }, function (err, csvFile) {
 		if (err)
 			return err;
 
-		console.log(data.data);
-		return res.render('csv.jade', { title: 'CSV File', csv: data });
+		if (csvFile) {
+			var schools = [],
+				headerAndInfo = [],
+				masterArray = [];
+
+			for (var i = 1; i < csvFile.data.length; i++) {
+				schools.push(csvFile.data[i][1]);
+			}
+
+
+			for (var x = 1; x < csvFile.data.length; x++) {
+				var headerInfoPair = {};
+				for (var y = 0; y < csvFile.data[x].length; y++) {
+					headerInfoPair[csvFile.data[0][y]] = csvFile.data[x][y];
+				}
+				headerAndInfo.push(headerInfoPair);
+			}
+		}
+
+		return res.render('csv.jade', { title: 'CSV File', schoolLinks: headerAndInfo });
 	});
 }
---
+++ app/public/assets/js/app.js
@@ -0,0 +1,4 @@
+$('#schools').accordion({
+	collapsible: true,
+	heightStyle: "fill"
+});
